f676dd06e12c20cc8bd3919ed15e0c293fca33cc
Harshit <harshitsoni2806@gmail.com>
2022-06-06 07:49:01 +0530

    .

---
+++ app/src/main/java/com/DataFlair/dataflairtodolist/task_database/TaskContract.java
@@ -0,0 +1,10 @@
+package com.DataFlair.dataflairtodolist.task_database;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.aziflaj.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    
+}
